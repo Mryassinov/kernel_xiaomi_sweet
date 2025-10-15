b065a0aaf1af220c635483372e3c849b908604ea
Devestrog <devestrog@gmail.com>
2025-10-16 00:39:20 +0300

    susFS integration

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Nov 6 01:17:06 +01 2025"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Nov 8 02:07:30 +01 2025"
 #define LINUX_COMPILE_BY "build-user"
 #define LINUX_COMPILE_HOST "build-host"
 #define LINUX_COMPILER "Android (13174946, +pgo, +bolt, +lto, +mlgo, based on r547379) clang version 20.0.0, LLD 20.0.0 (/mnt/disks/build-disk/src/android/llvm-r547379-release/out/llvm-project/llvm b718bcaf8c198c82f3021447d943401e3ab5bd54)"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.14.356-openela-splidder-ksu/df633ac3"
+#define UTS_RELEASE "4.14.356-openela-splidder-ksu/9e0b87ad"
