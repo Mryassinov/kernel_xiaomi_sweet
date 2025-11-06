df633ac37a008992493f46ff947678fc668e1511
Mryassinov <yassine.98.test4@gmail.com>
2025-11-06 01:14:39 +0100

    rebrand: Change kernel name from uhqqow to splidder

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 6 00:46:02 +01 2025"
+#define LINUX_COMPILE_BY "build-user"
+#define LINUX_COMPILE_HOST "build-host"
+#define LINUX_COMPILER "Android (13174946, +pgo, +bolt, +lto, +mlgo, based on r547379) clang version 20.0.0, LLD 20.0.0 (/mnt/disks/build-disk/src/android/llvm-r547379-release/out/llvm-project/llvm b718bcaf8c198c82f3021447d943401e3ab5bd54)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.14.356-openela-uhqqow-ksu/0ba6e867"
